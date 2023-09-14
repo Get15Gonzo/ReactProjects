684d63e7a7bcb78e6be3fb5fe1731582a169e6f1
Get15Gonzo <yourgithub@email.com>
2023-09-14 18:08:47 -0400

    Seperated componenets again and did some styling to the app

--- react-course-prj-1/src/App.js
+++ react-course-prj-1/src/App.js
@@ -1,38 +1,7 @@
 import './App.css';
-import logo from './logo.svg';
-
-function Header() {
-    return (
-    <header>
-        <nav>
-            <img src={logo} width="100px" />
-        </nav>
-    </header>
-    )
-}
-
-function MainContent() {
-    return(
-    <>
-    <h1>
-        My list
-    </h1>
-    <ul>
-        <li>Sick</li>
-        <li>Awesome</li>
-        <li>Cool</li>
-    </ul>
-    </>
-    )
-}
-
-function Footer() {
-    return(
-        <footer>
-            "copyright. Thomas Gonzales development. All rights reserved."
-        </footer>
-    )
-}
+import Header from './Header';
+import Footer from './Footer';
+import MainContent from './MainContent'
 
 function App() {
     return (
---
+++ react-course-prj-1/src/Footer.js
@@ -0,0 +1,9 @@
+function Footer() {
+    return(
+        <footer className="footer">
+            <small>c 2023 Thomas Gonzales Development. All rights reserved.</small>
+        </footer>
+    )
+}
+
+export default Footer;
