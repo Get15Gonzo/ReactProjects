618a19f6d4438195609dd86d3ea10bf248cd254a
Get15Gonzo <yourgithub@email.com>
2023-09-15 14:13:53 -0400

    Added styles to the page

--- react-course-prj-1/src/components/Navbar.js
+++ react-course-prj-1/src/components/Navbar.js
@@ -1,9 +1,11 @@
-function Navbar() {
+import Reactlogo from '../images/logo.svg';
+
+export default function Navbar() {
     return (
-    <h1>
-        Navbar goes here
-    </h1>
+        <nav>
+            <img src={Reactlogo} className="nav--icon"/>
+            <h3 className="nav--logo_text">ReactFacts</h3>
+            <h4 className="nav--title">React Course - Project 1</h4>
+        </nav>
     )
 }
-
-export default Navbar;
