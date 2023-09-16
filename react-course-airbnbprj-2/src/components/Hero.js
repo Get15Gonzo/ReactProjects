664f990cd1a60bfe9e85b4801df8b89d9a2fa3f3
Get15Gonzo <yourgithub@email.com>
2023-09-15 20:29:04 -0400

    Added the hero section to the airbnb project

---
+++ react-course-airbnbprj-2/src/components/Hero.js
@@ -0,0 +1,11 @@
+import collage from '../images/collage.png'
+
+export default function Hero() {
+    return (
+        <section className="hero">
+            <img src={collage} className="hero--collage" />
+            <h1 className="hero--title">Online Experiences</h1>
+            <p className="hero--text">Join unique interactive activites led by <br/> one-of-a-kind hosts - all without leaving <br/>home</p>
+        </section>
+    )
+}
