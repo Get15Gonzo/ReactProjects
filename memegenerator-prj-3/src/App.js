57f5d764b736f77bb6279ca88afadfa478a37a5a
Get15Gonzo <yourgithub@email.com>
2023-09-20 19:21:21 -0400

    Adding state

--- memegenerator-prj-3/src/App.js
+++ memegenerator-prj-3/src/App.js
@@ -1,8 +1,10 @@
+import React from "react"
 import './App.css';
 import Header from './components/Header'
 import Meme from './components/Meme'
 
 export default function App() {
+    React.useState()
     return (
         <>
             <Header />
--- memegenerator-prj-3/src/components/Meme.js
+++ memegenerator-prj-3/src/components/Meme.js
@@ -1,13 +1,14 @@
-import GreenButton from '../images/green-square.png'
+import MemesData from '../memes-data.js'
 
 export default function Meme() {
+
     return (
        <main>
-           <form className="form">
+           <div className="form">
                <input className="form--input" type="text" placeholder="Top text" />
                <input className = "form--input" type="text" placeholder="Bottom text" />
                <button className="form--button">Get a new meme image 🖼️</button>
-           </form>
+           </div>
        </main>
     )
 }
