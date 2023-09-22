94101e3e49d25d28fc99d312df4e6e5834dbf2da
Get15Gonzo <yourgithub@email.com>
2023-09-22 15:40:43 -0400

    Added state to the project

--- memegenerator-prj-3/src/components/Meme.js
+++ memegenerator-prj-3/src/components/Meme.js
@@ -1,13 +1,22 @@
-import MemesData from '../memes-data.js'
+import React from "react"
+import memesData from '../memes-data.js'
 
 export default function Meme() {
 
+    const [memeImage, setMemeImage] = React.useState("")
+    function getMemeImage() {
+        const memesArray = memesData.data
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        setMemeImage(memesArray[randomNumber].url)
+    }
+    let answer = isGoingOut ? "yes" : "no"
     return (
        <main>
            <div className="form">
                <input className="form--input" type="text" placeholder="Top text" />
                <input className = "form--input" type="text" placeholder="Bottom text" />
-               <button className="form--button">Get a new meme image 🖼️</button>
+               <button className="form--button" onClick={getMemeImage}>Get a new meme image 🖼️</button>
+               <img src={memeImage} className="meme--img" />
            </div>
        </main>
     )
