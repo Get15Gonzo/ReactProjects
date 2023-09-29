4925e5a2daabc9676c9ba42ce5b771390df60442
Get15Gonzo <yourgithub@email.com>
2023-09-29 12:16:00 -0400

    Adding light and dark mode to project 1

--- react-course-prj-1/src/App.js
+++ react-course-prj-1/src/App.js
@@ -2,12 +2,25 @@
 import './styles.css';
 import Navbar from './components/Navbar';
 import Main from './components/Main'
+import React from "react"
 
 function App() {
+
+    const [darkMode, setDarkMode] = React.useState(true)
+
+    function toggleDarkMode() {
+        setDarkMode(prevMode => !prevMode)
+    }
+
     return (
         <div className="container">
-            <Navbar />
-            <Main />
+            <Navbar 
+                darkMode={darkMode}
+                toggleDarkMode={toggleDarkMode}
+            />
+            <Main 
+                darkMode={darkMode}
+            />
         </div>
     )
 }
--- react-course-prj-1/src/components/Main.js
+++ react-course-prj-1/src/components/Main.js
@@ -1,6 +1,6 @@
-export default function Main() {
+export default function Main(props) {
     return(
-        <main>
+        <main className={props.darkMode ? "dark" : ""}>
             <h1 className="main--title">
                 Fun facts about React
             </h1>
